f711af39c9072b311b09ba9b299e82c43caefd5f
Nils Hansson <nilsmenghansson@gmail.com>
2024-07-23 09:34:23 +0200

    fixed errror

---
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -0,0 +1,18 @@
+import { getImage } from "~/server/queries";
+import { Modal } from "./modal";
+export default async function PhotoModal({
+  params: { id: photoId },
+}: {
+  params: { id: string };
+}) {
+  const idAsNumber = Number(photoId);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+  const image = await getImage(idAsNumber);
+  return (
+    <div>
+      <Modal>
+        <img src={image.url} className="w-96" />
+      </Modal>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,26 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
-import { getmyImages } from "~/server/queries";
+import { getImages } from "~/server/queries";
 import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/18e6d4fd-12b1-44fa-8b43-4cafa6ce1b2f-8sa96t.avif",
-  "https://utfs.io/f/ac843b57-bdf1-461f-aae1-8a976ade0f96-c1xv2i.jpg",
-  "https://utfs.io/f/11476007-108f-436d-9d16-0b06773cdbec-q57gk0.jpg",
-  "https://utfs.io/f/64d40a50-710d-4dcf-a2cd-1e7800af653a-tixmcn.jpg",
-];
-
 async function Images() {
-  const images = await getmyImages();
+  const images = await getImages();
   return (
     <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
         <div key={image.id} className="w-48">
-          <Image
-            src={image.url}
-            style={{ objectFit: "contain" }}
-            width={192}
-            height={192}
-            alt={image.name}
-          />
+          <Link href={`/img/${image.id}`}>
+            <Image
+              src={image.url}
+              style={{ objectFit: "contain" }}
+              width={192}
+              height={192}
+              alt={image.name}
+            />
+          </Link>
         </div>
       ))}
     </div>
