83650d41fbd4eca9fadfa20f652c21f2394a98d3
Nils Hansson <nilsmenghansson@gmail.com>
2024-07-23 10:51:27 +0200

    move to grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,23 @@ import Link from "next/link";
 import { db } from "~/server/db";
 import { getImages } from "~/server/queries";
 import Image from "next/image";
-
 export const dynamic = "force-dynamic";
-
 async function Images() {
   const images = await getImages();
   return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {images.map((image) => (
-        <div key={image.id} className="w-48">
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {[...images, ...images, ...images].map((image) => (
+        <div key={image.id} className="flex w-48 flex-col">
           <Link href={`/img/${image.id}`}>
             <Image
               src={image.url}
-              style={{ objectFit: "contain" }}
               width={192}
               height={192}
+              objectFit="contain"
               alt={image.name}
             />
           </Link>
+          <div>{image.name}</div>
         </div>
       ))}
     </div>
@@ -29,7 +28,7 @@ async function Images() {
 export default async function HomePage() {
   return (
     <main className="">
-      <SignedOut>Please sign in above</SignedOut>
+      <SignedOut>Please sign in bitch</SignedOut>
       <SignedIn>
         <Images />
       </SignedIn>
